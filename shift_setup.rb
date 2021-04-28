b876f171f5ec8af1c847f941de40d2763601bf11
daichi-fumoto <daichi.fumoto@di-v.co.jp>
2021-04-29 08:14:00 +0900

    コード修正
    重複表示を回避する機能の実装

--- shift_setup.rb
+++ shift_setup.rb
@@ -4,158 +4,152 @@ require "csv"
 require "date"
 
 # ============= CSV読み込み ============================ 
-data_list = CSV.read("CSV出力用.csv")
-start_member = "" # 誰から始めるか（最初から始める場合は空文字にしておく）
-last_member = ""  # 誰まで実行するか（最後まで実行する場合は空文字にしておく)
+csv_data     = CSV.read("CSV出力用.csv")
+@start_member = "" # 誰から始めるか（最初から始める場合は空文字にしておく）
+@last_member  = ""  # 誰まで実行するか（最後まで実行する場合は空文字にしておく)
 
 # ============= 画面を開く ============================ 
 options = Selenium::WebDriver::Chrome::Options.new
-driver = Selenium::WebDriver.for :chrome, options: options
-driver.navigate.to "https://atnd.ak4.jp/login"
-driver.manage.window.maximize
+@driver = Selenium::WebDriver.for :chrome, options: options
+@driver.navigate.to "https://atnd.ak4.jp/login"
+@driver.manage.window.maximize
+sleep 1
 
 # ============= AKASHIログイン ============================ 
+login_info = [{ form: 'form_company_id', env: ENV["AKASHI_ID"]      },
+              { form: 'form_login_id'  , env: ENV["AKASHI_EMAIL"]   },
+              { form: 'form_password'  , env: ENV["AKASHI_PASSWORD"]}]
+
+login_info.each do |info|
+  login_form = @driver.find_element(:id, info[:form])
+  login_form.send_keys info[:env]
+end
+
+@driver.find_element(:name, "commit").click
 sleep 1
-
-company_input = driver.find_element(:id, 'form_company_id')
-company_input.send_key ENV["AKASHI_ID"]
-
-email_input = driver.find_element(:id, 'form_login_id')
-email_input.send_keys ENV["AKASHI_EMAIL"]
-
-password_input = driver.find_element(:id, 'form_password')
-password_input.send_keys ENV["AKASHI_PASSWORD"]
-
-login_btn = driver.find_element(:name, "commit")
-login_btn.click
 
 # ============= 勤怠画面に遷移 ============================ 
-sleep 1
+month      = csv_data[0][5].to_i
+link_keys  = [{ attr: :xpath, key: "//p[text()=\"シフト\"]"        },
+              { attr: :xpath, key: "//a[text()=\"月次シフト\"]"     },
+              { attr: :id   , key: "date_chooser"                 },
+              { attr: :xpath, key: "//span[text()=\"#{month}月\"]"}]
 
-shift_link = driver.find_element(:xpath, "//p[text()=\"シフト\"]")
-shift_link.click
+link_keys.each do |link|
+  shift_page = @driver.find_element(link[:attr],link[:key])
+  shift_page.click
+end
 
-shift_link = driver.find_element(:xpath, "//a[text()=\"月次シフト\"]")
-shift_link.click
-
-shift_link = driver.find_element(:id, 'date_chooser')
-shift_link.click
-
-month = data_list[0][5].to_i
-shift_link = driver.find_element(:xpath, "//span[text()=\"#{month}月\"]")
-shift_link.click
+sleep 2
 
 # ============= 名前検索 & シフトマーク用意 ============================ 
-def shift_input(driver, data)
+def shift_input(data)
   if data[0] == "TRUE"
-    name = data[1]
-    name_link = driver.find_element(:xpath, "//p[text()=\"氏名\"]")
-    name_link.click
     
-    name_input = driver.find_element(:id, 'form_staff_name')
-    name_input.clear
-    name_input.send_keys name[0]
-    
-    name_btn = driver.find_element(:xpath, "//label[text()=\"#{name}\"]")
-
-    if @before_name != nil
-      if driver.find_element(:xpath, "//label[text()=\"#{@before_name}\"]").displayed? == false
-        driver.find_element(:xpath, "//label[text()=\"#{@before_name}\"]").click
-      end
-      name_btn.click
-    else
-      name_btn.click
-    end
+    @driver.find_element(:xpath, "//p[text()=\"氏名\"]").click
     sleep 1
 
-    @before_name = name
+    search_form = @driver.find_element(:id, 'form_staff_name')
+    search_form.clear
+    search_form.send_keys @name[0]
+    sleep 1
+
+    search_results = @driver.find_elements(:class,"c-table-filter__list__item--suggest")
+    @extra_names   = []
+
+    search_results.each do |result|
+      if result.displayed?
+        checkbox = result.find_element(:tag_name,"input")
+        @extra_names << result.find_element(:tag_name,"label").text if checkbox.selected? 
+      end
+    end
+
+    name_btn  = @driver.find_element(:xpath, "//label[text()=\"#{@name}\"]")
+    name_btn.click
+    sleep 1
     
-    # 早番入力
-    pattern = 'I（10時-19時）'
-    mark = ["10", "外A"]
+    if @extra_names != []
+      @extra_names.each do |e_name| 
+        @driver.find_element(:xpath, "//p[text()=\"氏名\"]").click
+        @driver.find_element(:id, 'form_staff_name').click
+        sleep 1
 
-    input(driver, data, pattern, mark)
+        extra_btn = @driver.find_element(:xpath, "//label[text()=\"#{e_name}\"]")
+        extra_btn.click
+        sleep 1
+      end
+    end
 
-    # 中番入力
-    pattern = 'K（11時-20時）'
-    mark = ["11-20", "外B"]
-
-    input(driver, data, pattern, mark)
-
-    # 遅番入力
-    pattern = 'O（13時-22時）'
-    mark = ["14-22", "19-22", "外C", "入", "拠16-22"]
-
-    input(driver, data, pattern, mark)
-
-    # 中抜け入力
-    pattern = 'Q（11時-22時）【中抜け】'
-    mark = ["11-13","11-22"]
-
-    input(driver, data, pattern, mark)
-
-    # 公休日入力
-    pattern = '公休'
-    mark = ["公"]
-
-    input(driver, data, pattern, mark)
-
-    # 有休日入力
-    pattern = '年次有給休暇'
-    mark = ["有"]
-
-    input(driver, data, pattern, mark)
-
-
+    mark_lists  = [{pattern: '公休'                 , mark: ["公"]            },
+                   {pattern: '年次有給休暇'          , mark: ["有"]            },
+                   {pattern: 'I（10時-19時）'        , mark: ["10", "外A"]     },
+                   {pattern: 'K（11時-20時）'        , mark: ["11-20", "外B"]  },
+                   {pattern: 'Q（11時-22時）【中抜け】', mark: ["11-13","11-22"]},
+                   {pattern: 'O（13時-22時）'        , mark: ["14-22", "19-22", "外C", "入", "16-22"]}]
+  
+    mark_lists.each do |list|
+      input(data, list[:pattern], list[:mark])
+    end
   end 
 end
 
 # ============= シフト入力処理 ============================ 
-def input(driver, data, pattern, mark) 
+def input(schedule, pattern, mark) 
 
-  select = Selenium::WebDriver::Support::Select.new(driver.find_element(:id, 'form_scheduled_template_id'))
-  select.select_by(:text, pattern) 
-  shift_cells = driver.find_elements(:class,"js-shift-calender-template-selectable-cell")
-  data.each_with_index do |shift, i|
+  pattern_list = Selenium::WebDriver::Support::Select.new(@driver.find_element(:id, 'form_scheduled_template_id'))
+  pattern_list.select_by(:text, pattern) 
+
+  cells  = @driver.find_elements(:class,"js-shift-calender-template-selectable-cell")
+  @judge = nil
+
+  schedule.each_with_index do |shift, i|
     if mark.any?{|m| shift.include?(m)}
-      cell = shift_cells[i-3]
-      driver.action.move_to(cell).perform
-      driver.action.move_by(0, 50).click.perform
+      target_cell = cells[i-3]
+      @driver.action.move_to(target_cell).perform
+      @driver.action.move_by(0, 50).click.perform
+      @judge = i
     end
+  end 
+
+  unless @judge == nil
+    apply  = @driver.find_element(:name, "button") 
+    apply.click 
+    sleep 1
   end
-  apply_link = driver.find_element(:name, "button")
-  apply_link.click
-  sleep 1
 end
 
 # ============= 終了処理 ============================ 
-def end_input(data, last_member)
-  if data[1] == last_member 
-    puts "#{data[1]}まで実行されたため終了します。"
+def end_input(data,n)
+
+  if @last_num == n
+    puts "--- All Green!! ---"
     exit
-  elsif last_member == ""
-    puts "#{data[1]}の処理は正常に完了しました。"
+  elsif @name == @last_member 
+    puts "LGTM ❀ #{@name}\n--- All Green!! ---"
+    exit
+  else 
+    puts "LGTM ❀ #{@name}" unless @name == nil
   end
 end
 
 # ============= 読み込み開始位置 ============================ 
-puts "#{month}月分の入力を開始します"
-data_list.each_with_index do |data, n| # csvの行を1行ずつ、取り出し、入力していく。
+puts "--- start #{month}month --- "
+csv_data.each_with_index do |data, n| # csvの行を1行ずつ、取り出し、入力していく。
+  @name     = data[1]
+  @last_num = csv_data.length-1
 
-  if data[0] == ""
-    puts "行情報が空のため自動登録を終了しました。"
-    
-  elsif start_member == ""
-    shift_input(driver, data)
-    end_input(data, last_member)
+  if @start_member == ""
+    shift_input(data)
+    end_input(data,n)
 
-  elsif data[1] == start_member
-    shift_input(driver, data)
-    end_input(data, last_member)
-    start_member = data_list[n+1][1]   # 次の人をstart_memmberに入れ、処理を回していく。
+  elsif @name == @start_member
+    shift_input(data)
+    # 次の人をstart_memmberに入れ、処理を回していく。
+    @start_member = csv_data[n+1][1] unless csv_data[n+1] == nil
+    end_input(data,n)
 
   else
-    puts "#{data[1]}の入力はskipしました"
+    puts "skip ✈ #{@name}"
   end
 end
 
